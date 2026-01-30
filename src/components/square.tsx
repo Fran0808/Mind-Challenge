cb02e88bccb9d4cc5f372e3249e6932ae1f33e0f
Fran0808 <fortegaquipse@gmail.com>
2026-01-30 10:59:26 -0500

    fix: correct component name casing

--- src/components/square.tsx
+++ src/components/square.tsx
@@ -1,5 +1,7 @@
-const square = () => {
-
+const Square = () => {
+    return(
+        <div className="w-full h-full bg-green-700 rounded-md cursor-pointer"></div>
+    )
 }
 
-export default square;
+export default Square;
