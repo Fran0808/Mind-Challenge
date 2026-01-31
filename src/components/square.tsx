e68025015a19d63173f519d343c55a67280be82b
Fran0808 <fortegaquipse@gmail.com>
2026-01-31 16:26:51 -0500

    feat: manejo de estado para el color

--- src/components/square.tsx
+++ src/components/square.tsx
@@ -1,9 +1,23 @@
+import { useState } from "react";
 import type { SquareInterface } from "../interfaces/Square";
 
-const Square = ({index, isActive, onClick}: SquareInterface) => {
-    return(
-        <div onClick={onClick} className={`w-full h-full ${isActive ? "bg-green-400": "bg-green-700"} rounded-md cursor-pointer `}></div>
-    )
-}
+const Square = ({isActive, onClick }: SquareInterface) => {
+  const [isClicked, setIsClicked] = useState<boolean>(false);
+
+  const handleColor = () => {
+    setIsClicked(true);
+    onClick();
+    setTimeout(() => setIsClicked(false), 200);
+  };
+
+  const isHighlighted = isActive || isClicked;
+
+  return (
+    <div
+      onClick={handleColor}
+      className={`w-full h-full ${isHighlighted ? "bg-green-400" : "bg-green-700"} rounded-md cursor-pointer `}
+    ></div>
+  );
+};
 
 export default Square;
