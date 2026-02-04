89373ebdd8b17eb1343afd4dfee5216417c7059c
Fran0808 <fortegaquipse@gmail.com>
2026-02-04 10:28:46 -0500

    feat: improving component for game initialization

--- src/components/StartScreen.tsx
+++ src/components/StartScreen.tsx
@@ -5,9 +5,11 @@ const StartScreen = () => {
     const [show, setShow] = useState<boolean>(false);
 
     return (
-        <div>
-            <button className="text-2xl cursor-pointer" onClick={()=>setShow(!show)}>Start</button>
-            {show && <Board/>}
+        <div className="flex justify-center items-center min-h-screen">
+            {!show &&
+                <button className="text-4xl bg-blue-600 text-white font-bold rounded-md pt-2 pb-2 pl-8 pr-8 cursor-pointer" onClick={() => setShow(true)}>Start</button>
+            }
+            {show && <Board />}
         </div>
     )
 }
