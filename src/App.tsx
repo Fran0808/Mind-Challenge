068a16d08e96a5d730da58a62192f2dcba7295ec
Fran0808 <fortegaquipse@gmail.com>
2026-02-07 11:38:30 -0500

    feat: adding background grid

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ import StartScreen from "./components/StartScreen"
 
 const App = () => {
   return (
-    <div>
+    <div className="relative flex flex-col justify-center items-center min-h-screen bg-linear-to-br from-slate-900 via-indigo-950 to-slate-900 overflow-hidden text-white">
       <StartScreen />
+      <div className="bg-overlay bg-grid"></div>
     </div>
   )
 }
