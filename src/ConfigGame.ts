1d4571f41670f4de9676e36a5e83a9e9ce99de07
Fran0808 <fortegaquipse@gmail.com>
2026-02-15 19:11:51 -0500

    feat: change difficulty settings

--- src/ConfigGame.ts
+++ src/ConfigGame.ts
@@ -3,17 +3,17 @@ export type Difficulty = "slow" | "medium" | "fast";
 export const GAME_CONFIG = {
     slow: {
         sequenceStartDelay: 1000,
-        squareDisplayDuration: 1000,
+        squareDisplayDuration: 900,
         nextLevelDelay: 600,
     },
     medium: {
         sequenceStartDelay: 800,
-        squareDisplayDuration: 700,
-        nextLevelDelay: 500,
+        squareDisplayDuration: 600,
+        nextLevelDelay: 400,
     },
     fast: {
         sequenceStartDelay: 600,
-        squareDisplayDuration: 500,
-        nextLevelDelay: 400,
+        squareDisplayDuration: 300,
+        nextLevelDelay: 200,
     },
 }
