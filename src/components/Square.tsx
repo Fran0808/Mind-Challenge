b1ea4b1789f99a8efea0ea48b7a737187155e68c
Fran0808 <fortegaquipse@gmail.com>
2026-02-18 23:21:17 -0500

    style: change color

--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -39,7 +39,7 @@ const Square = ({ index: _index, isActive, onClick, sound, isPlaying }: SquareIn
       onClick={handleColor}
       className={`w-full h-full rounded-md transition-all duration-75 ease-out
         ${isPlaying ? "cursor-default" : "cursor-pointer"} 
-        ${isHighlighted ? "bg-linear-to-t from-amber-300 to-orange-300" : "bg-linear-to-t from-amber-600 to-orange-600"}`}
+        ${isHighlighted ? "bg-linear-to-t from-amber-300 to-orange-400" : "bg-linear-to-t from-amber-600 to-orange-700"}`}
     ></div>
   );
 };
