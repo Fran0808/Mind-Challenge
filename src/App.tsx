baebb0e69c647cc5828f818e9ef871ad3c6596ac
Fran0808 <fortegaquipse@gmail.com>
2026-02-27 10:40:28 -0500

    feat: eliminate props unused

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ const App = () => {
   const [isMuted, setIsMuted] = useState<boolean>(false);
   return (
     <div className="relative flex flex-col justify-center items-center min-h-screen bg-linear-to-br from-slate-900 via-indigo-950 to-slate-900 overflow-hidden text-white">
-      <StartScreen isMuted={isMuted} setIsMuted={setIsMuted} />
+      <StartScreen isMuted={isMuted} />
       <div className="bg-overlay bg-grid animate-grid"></div>
       <SoundButton setIsMuted={setIsMuted} isMuted={isMuted} />
     </div>
