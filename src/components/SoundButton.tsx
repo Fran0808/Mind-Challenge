b4e8b5354376b63c84022ec3dd5d7271dec3fa4c
Fran0808 <fortegaquipse@gmail.com>
2026-02-27 10:32:33 -0500

    build: create SoundButton component

---
+++ src/components/SoundButton.tsx
@@ -0,0 +1,19 @@
+import { FaVolumeMute, FaVolumeUp } from "react-icons/fa";
+import type { StartScreenInterface } from "../interfaces/StartScreen";
+
+const SoundButton = ({ setIsMuted, isMuted }: StartScreenInterface) => {
+    return (
+        <button
+            onClick={() => { setIsMuted(!isMuted) }}
+            className="fixed bottom-8 left-8 cursor-pointer p-4 bg-white/5 backdrop-blur-xl rounded-2xl border border-white/10 hover:bg-white/10 hover:scale-110 active:scale-95 transition-all duration-300 z-50"
+        >
+            {isMuted ? (
+                <FaVolumeMute size={28} />
+            ) : (
+                <FaVolumeUp size={28} />
+            )}
+        </button>
+    )
+}
+
+export default SoundButton;
